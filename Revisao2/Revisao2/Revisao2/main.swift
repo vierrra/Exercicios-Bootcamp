f4ab7926604344694f5ab9534c4849aaff0728e7
vierrra <vierrra@hotmail.com>
2024-06-23 22:52:19 -0300

    first exercise solved

--- Revisao2/Revisao2/Revisao2/main.swift
+++ Revisao2/Revisao2/Revisao2/main.swift
@@ -7,5 +7,16 @@
 
 import Foundation
 
-print("Hello, World!")
+//1
+let listNumbers: [Int] = [1, 2, 3, 4, 5]
 
+func elementsSum(listNumbers: [Int]) -> Int {
+    var sum: Int = 0
+    for number in listNumbers {
+        sum += number
+    }
+    return sum
+}
+
+var result: Int = elementsSum(listNumbers: listNumbers)
+print("A soma dos elementos do array é: \(result)")
